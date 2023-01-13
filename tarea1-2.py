39f7bb0b2d645ffa2fd9c01dc62dc9d51c08b7a7
elias morales <eliasmorales.21@hotmail.com>
2023-01-13 19:57:35 -0300

    tarea 1 y 2

---
+++ tarea1-2.py
@@ -0,0 +1,12 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= "elias"
+>>> print a
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> print (a)
+elias
+>>> a=2
+>>> print (2)
+2
+>>> print (a)
+2
