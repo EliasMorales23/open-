68d7bfa5aaffe04e8b7261b26e9b3de25773d9f9
elias morales <eliasmorales.21@hotmail.com>
2023-01-15 20:35:10 -0300

    ejercicio 4

--- homework.py
+++ homework.py
@@ -1,3 +1,10 @@
+"""
+#Ejercicio 1
+elias = "Hola mundo"
+#Ejercicio 2
+elias = 2
+
+#Ejercicio 3
 Peso = int(input ("ingrese su peso en kilogramos "))
 
 altura = float(input("ingrese su altura en metros  "))
@@ -8,6 +15,23 @@ indice = (Peso / cuadrado)
 a= round(indice,2)
 
 print (f"su indice de masa corporal es de  {a}")
+
+"""
+#Ejercicio 4
+
+contador= 1
+lista=[]
+while contador <=100:
+    lista.append(contador)
+    contador+=1
+
+lista.reverse()
+print(lista)
+
+
+
+
+
 
 
 
