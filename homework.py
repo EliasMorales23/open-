07b6181ce9bc3962298ca028c14b338507bfa364
elias morales <eliasmorales.21@hotmail.com>
2023-01-15 18:43:39 -0300

    tarea 3

--- homework.py
+++ homework.py
@@ -1,2 +1,10 @@
-a= "hola mundo!" 
-print(a)
+Peso = int(input ("ingrese su peso en kilogramos "))
+
+altura = float(input("ingrese su altura en metros  "))
+
+cuadrado= altura**2 
+indice = (Peso / cuadrado)
+
+a= round(indice,2)
+
+print (f"su indice de masa corporal es de  {a}")
